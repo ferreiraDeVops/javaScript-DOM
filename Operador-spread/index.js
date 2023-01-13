ac39181e43c0c611f923f63d2ba7c2e356da8ec5
ferreiraDeVops <ferreiradevops@gmail.com>
2023-01-13 15:04:16 -0300

    Operador spread

--- Operador-spread/index.js
+++ Operador-spread/index.js
@@ -1,20 +1,34 @@
-const cidades = [`Brasilia`,`são paulo`,`minas`,`joao pessoa`,`maceio`]
+const cidades = [`Brasilia`,`São paulo`,`Minas`,`Joao pessoa`,`Maceio`]
+
 console.log(cidades)
+
+// Separa todos elementos e exibe os separadamente
 console.log(...cidades)
-console.log(...cidades[1])
+// Separa a string do indice indicado letra por letra
+console.log(...cidades[0])// como se fosse `b`,`r`,`a`,`s`,`i`,`l`,`i`,`a`
 
+// clonando um array
 const copyCidades = cidades
+// retirando ultimo elemento 
 copyCidades.pop()
 copyCidades.pop()
-copyCidades.push(`RIO DE JANEIRO`)
-console.log({cidades,copyCidades})
+copyCidades.pop()
+// adicionando um elmento ao final do array
+copyCidades.push(`Rio de janeiro`)
+// os dois arrays foram modificados
+console.log(cidades,copyCidades)
 
-
+// Criando um array de copy de forma correta que não modifique o original
+// modificações feitas nesse array ficam somente nele
 const cloneCidades = [...cidades]
+cloneCidades.push(`Rio grande do sul`)
+console.log(cidades,copyCidades,cloneCidades)
 
-cloneCidades.push(`Amazonas`)
-
-console.log({cidades,copyCidades,cloneCidades})
-
+// Criando um objeto apartir do array
 const cidadesObj = {...cidades}
-console.log({cidadesObj})
+
+//Criando um clone desse objeto
+const cidadesObjClone = {...cidadesObj}
+//passando um outro valor para o objeto
+cidadesObjClone.text = `Sobradinho`
+console.log({cidadesObj,cidadesObjClone})
