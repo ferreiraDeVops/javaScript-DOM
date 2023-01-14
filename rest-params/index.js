fd600df120086a7792cdce146f466100e9fc5e7e
ferreiraDeVops <ferreiradevops@gmail.com>
2023-01-14 11:05:36 -0300

    Treinamento rest-params

--- rest-params/index.js
+++ rest-params/index.js
@@ -1,5 +1,14 @@
-function sum(...number){
+// como seria na forma antiga 
+function summ(a,b,s,c,f){
+        return a+b+s+c+f
+}
+console.log(summ(2,3,4,5,6))
+// utilizando o rest params sempre no final da função
+function sum(parametros, ...number){
         // recebendo varios parametros para uma unica função
+        // pega o parametro number encadeia com um metodo de array reduce
+        // primeiro parametro sendo acumulador sendo o segundo parametro elemento atual
+        // somando o acumulador mais o numero, declarando que o acumulador começa em 0
         return number.reduce((accum,num) => accum + num , 0)
 }
 console.log(sum(1,1))
