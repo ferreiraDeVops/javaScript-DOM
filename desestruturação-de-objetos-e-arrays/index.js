d30e77923099495153f2753fa522aa42b40eec87
ferreiraDeVops <ferreiradevops@gmail.com>
2023-01-14 10:54:25 -0300

    Treino de desestruturação de objetos e arrays

--- desestruturação-de-objetos-e-arrays/index.js
+++ desestruturação-de-objetos-e-arrays/index.js
@@ -1,22 +1,30 @@
+
+//Criando objeto
 const person = {
-        name: `luke`,
-        job: `Farmer`,
-        parents: ["Anakin",`Padne`]
+        nome: `Luiz`,
+        job: `Dev`,
+        parents: ["Mãe",`Pai`]
 }
+// criando variaveis apartir da propriedade o Objeto
+// const nome = person.name
+// criando variaveis com desestruturação de array
+//quando o javascript ve as chaves ele ja sabe que tem que quebrar esse objeto
+// afrente so e preciso passar o nome do objeto que esta sendo referenciado para criação
+const {job , parents, nome}  = person
 
-const name = person.name
-const {job , parents}  = person
-
-console.log(name, job, parents)
-
+console.log(job, parents,nome)
+// Desestruturando um array
+// Quando e desestruturado eles tem que ficar na ordem do array  
 const [father , mother] = parents
+console.log(father,mother)
 
 
-const creatUser = ({name,job,parents}) =>{
+// desestruturando uma função pelos parametros
+const creatUser = ({nome,job,parents}) =>{
         const id = Math.floor(Math.random()*9999)
-        return{
+        return  {
                 id,
-                name,
+                nome,
                 job,
                 parents
         }
