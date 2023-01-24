410e63ef29556dcb7cdb1a722671bf6b17c4b8cb
ferreiraDeVops <ferreiradevops@gmail.com>
2023-01-24 20:57:33 -0300

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const miniCssExtractPlugin = require("mini-css-extract-plugin")
+
 const path = require('path') // caminhos no sistema operacional
 module.exports = {
         entry: { // Entrada do arquivo
@@ -8,8 +10,13 @@ module.exports = {
                 rules: [{ //  com as rules que significam as regras que o web pack vai usar 
                         test: /\.css$/, // expressoes regulares exteção do arquivo que vc quer passar
                         // acima vc pode passar a extenção para qualquer arquivo 
-                        use: ['style-loader','css-loader'] // qual loader sera usado
+                        //use: ['style-loader','css-loader'] // qual loader sera usado
+                        use: [miniCssExtractPlugin.loader, 'css-loader'] // remove o style loader e coloca o loader do proprio plugin
+
                 }]
-        }
+        },
+        plugins: [
+                new miniCssExtractPlugin()
+        ]
 }
 
